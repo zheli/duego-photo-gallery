70c86e05b1ce8a6403341b1534b9fb105b09c7f1
Zhe Li <linuxcity.jn@gmail.com>
2013-08-31 10:38:22 +0200

    API app structure and basic test

--- settings.py
+++ settings.py
@@ -135,6 +135,7 @@ INSTALLED_APPS = (
     # 'django.contrib.admindocs',
 
     #external
+    'rest_framework',
 
     #apps
     'apps.core',
@@ -168,6 +169,21 @@ LOGGING = {
             'propagate': True,
         },
     }
+}
+
+REST_FRAMEWORK = {
+    # Use hyperlinked styles by default.
+    # Only used if the `serializer_class` attribute is not set on a view.
+    'DEFAULT_MODEL_SERIALIZER_CLASS':
+        'rest_framework.serializers.HyperlinkedModelSerializer',
+
+    # Use Django's standard `django.contrib.auth` permissions,
+    # or allow read-only access for unauthenticated users.
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.AllowAny',
+    ),
+    'PAGINATE_BY': 10,
+    'PAGINATE_BY_PARAM': 'page_size',
 }
 
 DEBUG_APPS = None
